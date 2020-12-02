543e620eba2089ec7dc83ba0122dd75c49c98bd0
Mike Barlow <mike@mikebarlow.co.uk>
2020-12-02 09:14:15 +0000

    wip

--- resources/views/livewire/pages-admin.blade.php
+++ resources/views/livewire/pages-admin.blade.php
@@ -11,37 +11,41 @@
         </thead>
 
         <tbody class="bg-white">
-            @for($i=0 ; $i <= 4 ; $i++)
-                <tr>
-                    <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
-                        <div class="text-sm leading-5 font-medium text-gray-900">
-                            Homepage
-                        </div>
-                    </td>
+        @for($i=0 ; $i <= 4 ; $i++)
+            <tr>
+                <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
+                    <div class="text-sm leading-5 font-medium text-gray-900">
+                        Homepage
+                    </div>
+                </td>
 
-                    <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
-                        <div class="text-sm leading-5 text-gray-900">/</div>
-                    </td>
+                <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
+                    <div class="text-sm leading-5 text-gray-900">/</div>
+                </td>
 
-                    <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
+                <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
+                    @if (($i % 2) == 0)
                         <span class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800">Active</span>
-                    </td>
+                    @else
+                        <span class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-red-100 text-red-800">Inactive</span>
+                    @endif
+                </td>
 
-                    <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200 text-sm leading-5 text-gray-500">
-                        Dec 1st, 2020 22:54
-                    </td>
+                <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200 text-sm leading-5 text-gray-500">
+                    Dec 1st, 2020 22:54
+                </td>
 
-                    <td class="px-6 py-4 whitespace-no-wrap text-right border-b border-gray-200 text-sm leading-5 font-medium">
-                        <button class="px-6 py-3 bg-indigo-600 rounded-md text-white font-medium tracking-wide hover:bg-indigo-500 ml-3">
-                            <i class="fas fa-pencil-alt"></i>
-                        </button>
+                <td class="px-6 py-4 whitespace-no-wrap text-right border-b border-gray-200 text-sm leading-5 font-medium">
+                    <button class="p-2 bg-indigo-600 rounded-md text-white font-medium tracking-wide hover:bg-indigo-500 ml-3">
+                        <x-heroicon-s-pencil-alt class="w-4 h-4"/>
+                    </button>
 
-                        <button class="px-6 py-3 bg-red-600 rounded-md text-white font-medium tracking-wide hover:bg-red-500 ml-3">
-                            <i class="far fa-trash-alt"></i>
-                        </button>
-                    </td>
-                </tr>
-            @endfor
+                    <button class="p-2 bg-red-600 rounded-md text-white font-medium tracking-wide hover:bg-red-500 ml-3">
+                        <x-heroicon-s-trash class="w-4 h-4"/>
+                    </button>
+                </td>
+            </tr>
+        @endfor
         </tbody>
     </table>
 </div>
