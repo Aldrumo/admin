f6e32500c35b6a1b18d73aced3b070aca9f3e62e
Mike Barlow <mike@mikebarlow.co.uk>
2020-12-25 16:03:26 +0000

    wip

--- resources/views/livewire/Pages/edit-page.blade.php
+++ resources/views/livewire/Pages/edit-page.blade.php
@@ -43,16 +43,6 @@
         </div>
     @endif
     <script>
-        function initEditors()
-        {
-            BalloonEditor
-                // .create( document.getElementById('content-editor').contentDocument.querySelector('.content-editor') )
-                .create( document.querySelector('.tester') )
-                .catch( error => {
-                    console.error( error );
-                } );
-        }
-
         function editPage()
         {
             return {
