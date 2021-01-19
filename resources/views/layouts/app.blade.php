908730c45caa28243428a5ce252b9ef67c291484
Mike Barlow <mike@mikebarlow.co.uk>
2021-01-19 08:33:08 +0000

    wip

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -16,7 +16,7 @@
         @livewireStyles
 
         <!-- Scripts -->
-        <script src="{{ mix('js/app.js') }}" defer></script>
+        <script src="{{ secure_url('aldrumo/admin/js/app.js') }}" defer></script>
     </head>
     <body class="font-sans antialiased">
 
