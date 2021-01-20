8ccf1b200287ba95a2f88470584e76e50154f2e7
Mike Barlow <mike@mikebarlow.co.uk>
2021-01-20 19:27:49 +0000

    wip

--- resources/views/vendor/jetstream/components/welcome.blade.php
+++ resources/views/vendor/jetstream/components/welcome.blade.php
@@ -8,7 +8,7 @@
     </div>
 
     <div class="mt-6 text-gray-500">
-        Aldrumo is a <a href="https://tallstack.dev" rel="noopener" target="_blank" class="text-blue-950 hover:text-blue-500">TALL-Stack</a> CMS that doesn't take over or get in the way. You can continue to build your Laravel application without Aldrumo interferring.
+        Aldrumo is a <a href="https://tallstack.dev" rel="noopener" target="_blank" class="text-blue-950 hover:text-blue-500">TALL-Stack</a> CMS that doesn't take over or get in the way of your Laravel application.
     </div>
 </div>
 
@@ -49,11 +49,11 @@
 
             <a href="https://github.com/Aldrumo/core/discussions" class="text-blue-500 hover:text-blue-950">
                 <div class="mt-3 flex items-center text-sm font-semibold">
-                        <div>Ask a question!</div>
+                    <div>Ask a question!</div>
 
-                        <div class="ml-1">
-                            <svg viewBox="0 0 20 20" fill="currentColor" class="w-4 h-4"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
-                        </div>
+                    <div class="ml-1">
+                        <svg viewBox="0 0 20 20" fill="currentColor" class="w-4 h-4"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+                    </div>
                 </div>
             </a>
         </div>
@@ -67,7 +67,7 @@
 
         <div class="ml-12">
             <div class="mt-2 text-sm text-gray-500">
-                If you've found a bug? We'd like to know!<br>
+                Have you found a bug? We'd like to know!<br>
                 Post up a report on Github and we'll take a look!
             </div>
 
