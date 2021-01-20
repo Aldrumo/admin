c2b592ceb66229e10132597b46eb68abfc36ddab
Mike Barlow <mike@mikebarlow.co.uk>
2021-01-20 07:38:00 +0000

    wip

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,11 @@ module.exports = {
             fontFamily: {
                 sans: ['Nunito', ...defaultTheme.fontFamily.sans],
             },
+            colors: {
+                blue: {
+                    950: '#14213D'
+                },
+            },
         },
     },
 
