01932bee4ebf26b161fc86e5e57e9badeaa516fe
Mike Barlow <mike@mikebarlow.co.uk>
2021-01-24 21:33:39 +0000

    wip

--- src/Http/Middleware/InjectEditor.php
+++ src/Http/Middleware/InjectEditor.php
@@ -41,8 +41,8 @@ class InjectEditor
                             inline: true,
                             menubar: false,
                             plugins: "link image",
-                            toolbar: "undo redo | alignnone alignleft aligncenter alignright alignjustify |" .
-                             "styleselect | backcolor forecolor | bold italic | link image",
+                            toolbar: "undo redo | alignnone alignleft aligncenter alignright alignjustify |' .
+                             'styleselect | backcolor forecolor | bold italic | link image",
                             image_advtab: true,
                         });
                     }
