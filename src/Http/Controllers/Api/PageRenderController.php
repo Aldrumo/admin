c790ebaf983213b642b268727994b54271212217
Mike Barlow <mike@mikebarlow.co.uk>
2021-01-26 08:41:00 +0000

    wip

--- src/Http/Controllers/Api/PageRenderController.php
+++ src/Http/Controllers/Api/PageRenderController.php
@@ -3,12 +3,21 @@
 namespace Aldrumo\Admin\Http\Controllers\Api;
 
 use Aldrumo\Core\Models\Page;
+use Aldrumo\ThemeManager\ThemeManager;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Illuminate\Support\Facades\View;
 
 class PageRenderController
 {
+    /** @var ThemeManager */
+    protected $themeManager;
+
+    public function __construct(ThemeManager $themeManager)
+    {
+        $this->themeManager = $themeManager;
+    }
+
     public function __invoke(Request $request, int $id)
     {
         $page = Page::with('blocks')
@@ -20,6 +29,7 @@ class PageRenderController
         ]);
 
         return view(
+            $this->themeManager->activeTheme()->packageName() . '::' .
             $page->template,
             [
                 'page' => $page,
